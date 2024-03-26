1e5f98b80c80c8f3a69c3f95ca7f269351782891
ChernenkoAndriy <andre.chrn@gmail.com>
2024-03-26 14:28:38 +0200

    changed reading from file to stdin.  Added char distinguishing

--- Project.asm
+++ Project.asm
@@ -1,109 +1,77 @@
 .model small
-.stack 100h
+.stack 3h
+
 .data
-keys db 10000*16 dup(?)
-values db 10000*16 dup(?)
-curWord db 16 dup('$') 
-oneChar db ?
-filename db 'inp.txt', 0
-handle dw ?
-сurkey db 0
-curnum db 0
+tempChar db 0
+keys db 10000*16 dup(0)
+num_Values dw 10000 dup(0)
+isWord db 1
+temp_Key_Ar db 16 dup(0)
+key_Temp_Ind dw 0
+temp_value db 16 dup(0)
 .code
-start:
+main proc
     mov ax, @data
-    mov ds, ax   
-    
-    ; Open the file for reading
-    mov ah, 3Dh        ; Function to open file
-    lea dx, filename   ; Load filename
-    mov al, 0          ; Read-only mode
-    int 21h            ; AX will contain file handle
-    mov handle, ax     ; Save file handle
-    
+    mov ds, ax
+ 
+
 read_next:
-    ; Read from file
-    mov oneChar, 32
-    mov ah, 3Fh        ; Function to read from file
-    mov bx, handle     ; File handle
-    mov cx, 1          ; Number of bytes to read
-    lea dx, oneChar    ; Pointer to buffer
-    int 21h
+    mov ah, 3Fh
+    mov bx, 0 
+    mov cx, 1  
+    mov dx, offset tempChar   
+    int 21h   
     push ax
-    mov ah, 02h
+    push bx
+    push cx
     push dx
-    mov dl, oneChar
-    int 21h        
-    pop dx     ; Read from file 
-    pop ax     ; Check for errors
-    cmp ax, 0          ; Check if end of file
-    je end_of_file     ; Jump if end of file reached
-    
-    jmp write_char
-          
-
-display_char:
-    push ax
-    mov ah, 02h
-    push dx
-    mov dl, oneChar
-    int 21h        
-    pop dx     
+    call work_with_char 
+    pop dx
+    pop cx
+    pop bx
     pop ax
-    
-end_of_file:
-    mov ah, 01h
+    cmp ax, 0
+    jnz read_next
+    mov ax, 4C00h
     int 21h
-    ; Handle end of file here
-    jmp exit_program
 
-exit_program:
-    mov ah, 4Ch        ; Function to terminate program
-    int 21h 
-    
-write_char:
-push ax
-push si
-push cx
-push di
-cmp oneChar,  32
-je writekey
-cmp oneChar,  'a'
-je writeNum
-mov si, offset curWord ; Завантажуємо адресу початку масиву curWord у SI
-    mov cx, 16             ; CX = довжина масиву curWord
-search_free_space:
-    lodsb                 ; Завантажуємо наступний символ з масиву curWord в AL
-    cmp al, '$'           ; Перевіряємо, чи це кінець рядка
-    je found_space        ; Якщо так, ми знайшли вільне місце
-    loop search_free_space ; Якщо ні, продовжуємо пошук
-    jmp end_of_file       ; Якщо не знайдено вільного місця, просто завершуємо функцію
+main endp
 
-found_space:
-    dec si                  ; Повертаємося назад, щоб записати символ на вільне місце
-    mov al, oneChar        ; Завантажуємо значення oneChar у AL
-    mov [si], al           ; Записуємо AL у вільне місце
-    jmp read_next          ; Переходимо до наступного символу з файлу
-pop di
-pop cx
-pop si
-pop ax
-jmp read_next
+work_with_char proc
+cmp tempChar,0Dh
+jnz notCR
+cmp isWord,0
+jne endProc
+mov isWord,1
+;code to transform char in number
+jmp endProc
+notCR:
+cmp tempChar,0Ah
+jnz notLF
+cmp isWord,0
+jnz endProc
+mov isWord,1
+;code to transform char in number
+jmp endProc
+notLF:
+cmp tempChar,20h
+jnz notSpace
+mov isWord,0
+    jmp endProc
+notSpace:
+    cmp isWord, 0
+    jnz itsWord
+       lea si, temp_value 
+        mov bx, numberInd
+        add si, bx
+        mov al, tempChar
+        mov [si], al
+        inc numberInd
+          jmp endProc
+itsWord:
+       
 
-writekey:
-push ax
-push bx
-push cx
-push dx
+endProc:
+    ret
+ work_with_char endp
 
-pop dx 
-pop cx
-pop bx
-pop ax
-
-
-writenum:
-
-
-
-end start
